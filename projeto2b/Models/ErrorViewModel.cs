3f7502aa0d74835d13ec0f456f0d424496cdbe44
LARISSA <larissavitbc@gmail.com>
2025-03-19 19:57:41 -0300

    criando um projeto de login

---
+++ projeto2b/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projeto2b.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
